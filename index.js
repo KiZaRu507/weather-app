0383cefbf00456e7409843d711eebb07c38cf988
Sumukh VR <vrsumukh32@gmail.com>
2022-01-04 16:21:59 +0530

    Fixing the vulnerability

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const textbox=document.querySelector('.data');
 const button = document.querySelector('button');
 const user=document.querySelector('.user-input');
 const res=document.querySelector('.results')
+require('dotenv').config();
+
 // Added the event listener on submit btn to fetch the value enterd by the user after user clicks on submit
 button.addEventListener('click',()=>{
     console.log(textbox.value);
@@ -10,7 +12,7 @@ button.addEventListener('click',()=>{
     hello
     `);
     
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${textbox.value}&appid=4e79ddf88f9c1157bd3d182521503b47&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${textbox.value}&appid=${process.env.API_KEY}&units=metric`)
   .then(response => response.json())
   .then(data => console.log(data['main']['temp']));
 
